edc886d86a9663d0052e3048ee49f81c408129d6
GabrielAgrela <gabriel.agrela@hotmail.com>
2024-03-07 08:03:12 -0300

    feat: register form

---
+++ src/app/register/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import RegisterForm from "@/components/RegisterForm";
+import React from "react";
+import styled from "styled-components";
+
+const Register: React.FC = () => {
+  return (
+    <RegisterContainer>
+      <Title>Register</Title>
+      <RegisterForm />
+    </RegisterContainer>
+  );
+};
+
+const RegisterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 95dvh;
+`;
+
+const Title = styled.p`
+  font-size: 28px;
+  padding-bottom: 20px;
+`;
+
+export default Register;
---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,145 @@
+"use client";
+
+import React from "react";
+import { Button, Form as AntdForm, Input } from "antd";
+import styled from "styled-components";
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+const RegisterForm: React.FC = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: "email",
+            message: "The input is not valid E-mail!",
+          },
+          {
+            required: true,
+            message: "Please input your E-mail!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: "Password must be at least 4 characters",
+            min: 4,
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={["password"]}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please confirm your password!",
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue("password") === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(
+                new Error("The new password that you entered do not match!")
+              );
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="username"
+        label="Username"
+        rules={[
+          {
+            required: true,
+            message: "Username must be at least 4 characters!",
+            whitespace: true,
+            min: 4,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item shouldUpdate {...tailFormItemLayout}>
+        {() => (
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={
+              !form.isFieldsTouched(true) ||
+              !!form.getFieldsError().filter(({ errors }) => errors.length)
+                .length
+            }
+          >
+            Log in
+          </Button>
+        )}
+      </Form.Item>
+    </Form>
+  );
+};
+
+const Form = styled(AntdForm)`
+  @media (max-width: 576px) {
+    width: 80%;
+  }
+  max-width: 650px;
+  width: 500px;
+`;
+
+export default RegisterForm;
