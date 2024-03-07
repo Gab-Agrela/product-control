b2d180c0e9a23caafd94d9fa1d51543ffbfdcbfe
GabrielAgrela <gabriel.agrela@hotmail.com>
2024-03-07 09:21:47 -0300

    feat: register query done

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React from "react";
-import { Button, Form as AntdForm, Input } from "antd";
+import React, { useEffect } from "react";
+import { Button, Form as AntdForm, Input, message } from "antd";
 import styled from "styled-components";
+import { useRegisterAccountQueryMutation } from "@/modules/rtk/accountsQuery";
 
 const formItemLayout = {
   labelCol: {
@@ -30,107 +31,137 @@ const tailFormItemLayout = {
 
 const RegisterForm: React.FC = () => {
   const [form] = Form.useForm();
+  const [trigger, { data, isError, error }] = useRegisterAccountQueryMutation();
+  const [messageApi, contextHolder] = message.useMessage();
+
+  useEffect(() => {
+    if (isError && error) {
+      messageApi.open({
+        type: "error",
+        content: (error as any)?.data?.message || "Error when creating account",
+        duration: 3,
+      });
+    }
+
+    if (data && !isError) {
+      messageApi.open({
+        type: "success",
+        content: (data as any)?.message,
+        duration: 3,
+      });
+
+      form.resetFields();
+    }
+  }, [isError, data, error, messageApi, form]);
 
   const onFinish = (values: any) => {
-    console.log("Received values of form: ", values);
+    const { email, password, username } = values;
+    trigger({
+      email: email.trim(),
+      password: password.trim(),
+      username: username.trim(),
+    });
   };
 
   return (
-    <Form
-      {...formItemLayout}
-      form={form}
-      name="register"
-      onFinish={onFinish}
-      scrollToFirstError
-    >
-      <Form.Item
-        name="email"
-        label="E-mail"
-        rules={[
-          {
-            type: "email",
-            message: "The input is not valid E-mail!",
-          },
-          {
-            required: true,
-            message: "Please input your E-mail!",
-          },
-        ]}
+    <>
+      {contextHolder}
+      <Form
+        {...formItemLayout}
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        scrollToFirstError
       >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: "Password must be at least 4 characters",
-            min: 4,
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name="confirm"
-        label="Confirm Password"
-        dependencies={["password"]}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please confirm your password!",
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue("password") === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(
-                new Error("The new password that you entered do not match!")
-              );
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
             },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        name="username"
-        label="Username"
-        rules={[
-          {
-            required: true,
-            message: "Username must be at least 4 characters!",
-            whitespace: true,
-            min: 4,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: "Password must be at least 4 characters",
+              min: 4,
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item shouldUpdate {...tailFormItemLayout}>
-        {() => (
-          <Button
-            type="primary"
-            htmlType="submit"
-            disabled={
-              !form.isFieldsTouched(true) ||
-              !!form.getFieldsError().filter(({ errors }) => errors.length)
-                .length
-            }
-          >
-            Log in
-          </Button>
-        )}
-      </Form.Item>
-    </Form>
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("The new password that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="username"
+          label="Username"
+          rules={[
+            {
+              required: true,
+              message: "Username must be at least 4 characters!",
+              whitespace: true,
+              min: 4,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item shouldUpdate {...tailFormItemLayout}>
+          {() => (
+            <Button
+              type="primary"
+              htmlType="submit"
+              disabled={
+                !form.isFieldsTouched(true) ||
+                !!form.getFieldsError().filter(({ errors }) => errors.length)
+                  .length
+              }
+            >
+              Register
+            </Button>
+          )}
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
